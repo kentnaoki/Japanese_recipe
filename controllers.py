2970dc2ccbbc0db4da3762f9d0797c73c89c3740
naoki <ueda.191201@gmail.com>
2022-01-04 23:24:24 +0000

    Change before login form

--- controllers.py
+++ controllers.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+import db
+from models import User, Task
 
 app = FastAPI(
     title = "Japanese food recipe",
@@ -7,5 +10,15 @@ app = FastAPI(
     version = "0.9 beta"
 )
 
+templates = Jinja2Templates(directory="templates")
+jinja_env = templates.env
+
 def index(request: Request):
-    return {"Hello", "World"}
+    return templates.TemplateResponse("index.html", {"request": request}, )
+
+def admin(request: Request):
+    user = db.session.query(User).filter(User.username == "admin").first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+
+    return templates.TemplateResponse("admin.html", {"request": request, "user": user, "task": task})
